0b7576680546bd3f02a689c5a9d83391b56dc32a
polikoushe <koushalieva@gmail.com>
2016-09-26 14:31:35 +0300

    boolean var

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class BooleanVariable
+{
+    static void Main()
+    {
+
+
+        string str1 = Console.ReadLine(); 
+
+        bool IsTrue = str1 == "True";
+
+        if (IsTrue)
+        { Console.WriteLine("Yes");
+
+        }
+
+        else
+        {
+            Console.WriteLine("No");
+        }
+
+
+       
+
+    }
+}
